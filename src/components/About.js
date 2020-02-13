7010e35afc8adea5f18e518ba9ad270351c544ec
gavin stahl <stahlgavin@gmail.com>
2020-02-13 14:35:08 -0800

    about section almost done

--- src/components/About.js
+++ src/components/About.js
@@ -10,10 +10,10 @@ export default function About() {
         <h2>Full Stack Web Developer</h2>
         <p>Redondo Beach, CA</p>
         <p>StahlGavin@gmail.com</p>
+        <p>Leveling up my skills in order to be the best developer I can be. Currently attending and working at Lambda School.</p><br/>
         <a rel="external noopener noreferrer" href="https://www.linkedin.com/in/gavin-stahl-0842b6184/" target="_blank">Linked-In</a>
         <a rel="external noopener noreferrer" href="https://github.com/stahlgazer" target="_blank">Github</a>
         <a rel="external noopener noreferrer" href="https://twitter.com/stahlgazer" target="_blank">Twitter</a>
-        <p>Leveling up my skills in order to be the best developer I can be. Currently attending and working at Lambda School.</p>
       </div>
     </section>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="footer">
-        <p>Copyright &#169; 2020 by Gavin Stahl</p>
+        <p>Copyright &#169; 2020 by Gavin Stahl using React.js</p>
     </div>
   );
 }
