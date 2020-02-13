d39ae4e366f3c2ecbb640a5e98cb2da9c7c09c8a
gavin stahl <stahlgavin@gmail.com>
2020-02-13 12:45:34 -0800

    updated form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,24 @@
 import React from "react";
 
 export default function Contact() {
+
+const handleSubmit = e => {
+e.preventDefault()
+}
+
   return (
     <div id="Contact">
       <h2>Contact</h2>
       <form>
         <div>
-        <label>Name: </label>
-        <input type="text" />
-        <label>E-mail: </label>
-        <input type="text" />
+          <label>Name: </label>
+          <input type="name" required />
+          <label>E-mail: </label>
+          <input type="email" required />
         </div>
         <label>Message: </label>
-        <input type="text" />
-        <button type='submit'>Submit</button>
+        <input type="text" required />
+        <button type="submit" onSubmit={handleSubmit}>Submit</button>
       </form>
     </div>
   );
