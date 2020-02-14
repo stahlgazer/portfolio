217f85b8ff667152e6e01ad9066dff1771c28cd5
gavin stahl <stahlgavin@gmail.com>
2020-02-13 17:44:48 -0800

    icons added for skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,7 +4,35 @@ export default function Skills() {
   return (
     <section id="Skills">
       <div className="nav-fix">
-        <h2>skills</h2>
+        <h2>Skills - Languages - Technologies</h2>
+      </div>
+      <div className="icons-container">
+        <div>
+          <i class="fab fa-html5"></i>
+        </div>
+        <div>
+          <i class="fab fa-css3-alt"></i>
+        </div>
+        <div>
+          <i class="fab fa-js-square"></i>
+        </div>
+        <div>
+          <i class="fab fa-react"></i>
+        </div>
+      </div>
+      <div>
+        <div className="icons-container">
+          <i class="fab fa-sass"></i>
+        </div>
+        <div>
+          <i class="fab fa-github"></i>
+        </div>
+        <div>
+          <i class="fab fa-node"></i>
+        </div>
+        <div>
+          <i class="fab fa-bootstrap"></i>
+        </div>
       </div>
     </section>
   );
