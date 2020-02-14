f590f30a53f3f4263bc8ab1640fb80abf352b4d9
gavin stahl <stahlgavin@gmail.com>
2020-02-14 15:23:50 -0800

    working on project template

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,6 +7,11 @@ export default function Projects() {
       <div className="nav-fix">
         <h2>Projects</h2>
       </div>
+      <div className="projects-container">
+        <div className="single-project"><h2>Project Title</h2></div>
+        <div className="single-project"><h2>Project Title</h2></div>
+        <div className="single-project"><h2>Project Title</h2></div>
+      </div>
       <div className="calendar" id="Contributions">
         <GitHubCalendar username="stahlgazer" color="hsl(203, 82%, 33%)" />
       </div>
