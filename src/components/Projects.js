4f9920514a1f0d8c7f348d9e27a455a86cc620de
gavin stahl <stahlgavin@gmail.com>
2020-02-15 20:51:11 -0800

    added project headings styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -19,10 +19,6 @@ export default function Projects() {
           <h2>To Do List</h2>
           <img src={todo} alt="project2" />
         </div>
-        <div className="single-project">
-          <h2>Project Title3</h2>
-          <img src="" alt="project3" />
-        </div>
       </div>
       <div className="calendar" id="Contributions">
         <GitHubCalendar username="stahlgazer" color="hsl(203, 82%, 33%)" />
