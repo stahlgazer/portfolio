6328907a051e56fdae66ac4fa22e97e2fa9279b5
gavin stahl <stahlgavin@gmail.com>
2020-02-15 20:12:17 -0800

    added project images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,9 +8,18 @@ export default function Projects() {
         <h2>Projects</h2>
       </div>
       <div className="projects-container">
-        <div className="single-project"><h2>Project Title</h2></div>
-        <div className="single-project"><h2>Project Title</h2></div>
-        <div className="single-project"><h2>Project Title</h2></div>
+        <div className="single-project">
+          <h2>Project Title1</h2>
+          <img src="../images/tipsease.PNG" alt="project1" />
+        </div>
+        <div className="single-project">
+          <h2>Project Title2</h2>
+          <img src="../images/todoapp2.PNG" alt="project2" />
+        </div>
+        <div className="single-project">
+          <h2>Project Title3</h2>
+          <img src="" alt="project3" />
+        </div>
       </div>
       <div className="calendar" id="Contributions">
         <GitHubCalendar username="stahlgazer" color="hsl(203, 82%, 33%)" />
