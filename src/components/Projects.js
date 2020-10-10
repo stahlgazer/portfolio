5780aa26a6706ad05ead6f5c409bb68c23c97005
Gavin S <stahlgavin@gmail.com>
2020-10-09 22:18:43 -0700

    projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,9 @@ import React from "react";
 import GitHubCalendar from "react-github-calendar";
 import tipsease from "../images/tipsease.PNG";
 import todo from "../images/todoapp2.PNG";
-import ReactTooltip from 'react-tooltip'
+import calc from "../images/calculator.PNG";
+import game from "../images/gameoflife.PNG";
+import ReactTooltip from "react-tooltip";
 
 export default function Projects() {
   const exampleTheme = {
@@ -22,20 +24,44 @@ export default function Projects() {
       </div>
       <div className="projects-container">
         <div className="single-project">
-          <h2>Tipsease</h2>
-          <a target="blank" href="https://awesome-jackson-f31166.netlify.com/">
-            <img src={tipsease} alt="project1" />
+          <h2>Grid Calculator</h2>
+          <a target="blank" href="https://grid-calculator.vercel.app/">
+            <img src={calc} alt="calculator preview" />
           </a>
           <h3>Project Details</h3>
-          <p>Role: test</p>
+          <p>Role: Coming Soon</p>
+          <p>Description</p>
+          <p>Github</p>
+        </div>
+        <div className="single-project">
+          <h2>Game of Life</h2>
+          <a target="blank" href="https://game-of-life-smoky-zeta.vercel.app/">
+            <img src={game} alt="game of life preview" />
+          </a>
+          <h3>Project Details</h3>
+          <p>Role: Coming Soon</p>
+          <p>Description</p>
+          <p>Github</p>
+        </div>
+        <div className="single-project">
+          <h2>Tipsease</h2>
+          <a target="blank" href="https://awesome-jackson-f31166.netlify.com/">
+            <img src={tipsease} alt="tipsease page preview" />
+          </a>
+          <h3>Project Details</h3>
+          <p>Role: Coming Soon</p>
+          <p>Description</p>
+          <p>Github</p>
         </div>
         <div className="single-project">
           <h2>To Do List</h2>
           <a target="blank" href="https://react-todo-sigma.now.sh/">
-            <img src={todo} alt="project2" />
+            <img src={todo} alt="to do list preview" />
           </a>
           <h3>Project Details</h3>
-          <p>Role: test</p>
+          <p>Role: Coming Soon</p>
+          <p>Description</p>
+          <p>Github</p>
         </div>
       </div>
       <div className="calendar" id="Contributions">
