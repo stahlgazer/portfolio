0158ed9cfe072a773b3f5991e59518e7e1e82f1d
Gavin S <stahlgavin@gmail.com>
2020-11-17 23:21:09 -0800

    media queries

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,89 +11,79 @@ export default function Skills() {
       <div>
         <h2>Skills / Languages</h2>
       </div>
-      <div
-        data-aos="fade-down-right"
-        data-aos-duration="1000"
-        data-aos-delay="300"
-        className="icons-container"
-        data-aos-mirror="true"
-      >
-        <div className="icondiv">
-          <i className="fab fa-html5"></i>
-          <p>Html5</p>
+      <div className="mobile-icons-container">
+        <div
+          data-aos="fade-down-right"
+          data-aos-duration="1000"
+          data-aos-delay="300"
+          className="icons-container"
+          data-aos-mirror="true"
+        >
+          <div className="icondiv">
+            <i className="fab fa-html5"></i>
+            <p>Html5</p>
+          </div>
+          <div className="icondiv">
+            <i className="fab fa-css3-alt"></i>
+            <p>CSS3</p>
+          </div>
+          <div className="icondiv">
+            <i className="fab fa-js-square"></i>
+            <p>JavaScript</p>
+          </div>
+          <div className="icondiv">
+            <i className="fab fa-sass"></i>
+            <p>SCSS</p>
+          </div>
+          <div className="icondiv">
+            <i class="fab fa-less"></i>
+            <p>LESS</p>
+          </div>
+          <div className="icondiv">
+            <i className="fab fa-bootstrap"></i>
+            <p>Bootstrap</p>
+          </div>
+          <div className="icondiv">
+            <img alt="MaterialUI Logo" src={mui} />
+            <p>Material-UI</p>
+          </div>
         </div>
-        <div className="icondiv">
-          <i className="fab fa-css3-alt"></i>
-          <p>CSS3</p>
-        </div>
-        <div className="icondiv">
-          <i className="fab fa-js-square"></i>
-          <p>JavaScript</p>
-        </div>
-        <div className="icondiv">
-          <i className="fab fa-sass"></i>
-          <p>SCSS</p>
-        </div>
-        <div className="icondiv">
-          <i class="fab fa-less"></i>
-          <p>LESS</p>
-        </div>
-        <div className="icondiv">
-          <i className="fab fa-bootstrap"></i>
-          <p>Bootstrap</p>
-        </div>
-        <div className="icondiv">
-          <img alt="MaterialUI Logo" src={mui} />
-          <p>Material-UI</p>
-        </div>
-      </div>
-      <div
-        data-aos="fade-up-left"
-        data-aos-delay="300"
-        data-aos-duration="1000"
-        className="icons-container"
-        data-aos-mirror="true"
-      >
-        <div className="icondiv">
-          <i className="fab fa-python"></i>
-          <p>Python</p>
-        </div>
-        <div className="icondiv">
-          <i className="fab fa-react"></i>
-          <p>React.js</p>
-        </div>
-        <div className="icondiv">
-          <img
-            alt="Redux Logo"
-            src={redux}
-          />
-          <p>Redux.js</p>
-        </div>
-        <div className="icondiv">
-          <i className="fab fa-node-js"></i>
-          <p>Node.js</p>
-        </div>
-        <div className="icondiv">
-          <img
-            alt="GraphQL Logo"
-            src={graphql}
-          />
-          <p>GraphQL</p>
-        </div>
+        <div
+          data-aos="fade-up-left"
+          data-aos-delay="300"
+          data-aos-duration="1000"
+          className="icons-container"
+          data-aos-mirror="true"
+        >
+          <div className="icondiv">
+            <i className="fab fa-python"></i>
+            <p>Python</p>
+          </div>
+          <div className="icondiv">
+            <i className="fab fa-react"></i>
+            <p>React.js</p>
+          </div>
+          <div className="icondiv">
+            <img alt="Redux Logo" src={redux} />
+            <p>Redux.js</p>
+          </div>
+          <div className="icondiv">
+            <i className="fab fa-node-js"></i>
+            <p>Node.js</p>
+          </div>
+          <div className="icondiv">
+            <img alt="GraphQL Logo" src={graphql} />
+            <p>GraphQL</p>
+          </div>
 
-        <div className="icondiv">
-          <img
-            alt="SQL Logo"
-            src={sql}
-          />
-          <p>SQL</p>
-        </div>
-        <div className="icondiv">
-          <img
-            alt="Git Logo"
-            src={git}
-          />
-          <p>Git</p>
+          <div className="icondiv">
+            <img alt="SQL Logo" src={sql} />
+            <p>SQL</p>
+          </div>
+          <div className="icondiv">
+            <img alt="Git Logo" src={git} />
+            <p>Git</p>
+          </div>
         </div>
       </div>
     </section>
